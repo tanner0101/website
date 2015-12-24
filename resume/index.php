75600d6043f9974d6457ae6fe09265e554cbae0a
Tanner Nelson <tanner@bluebite.com>
2015-12-23 22:53:18 -0500

    initial commit

---
+++ resume/index.php
@@ -0,0 +1,5 @@
+<?php
+
+   header( 'Location: https://www.icloud.com/pages/AwBUCAESELgM3zqLz2sCa1OJeR5EyLsaKZwiPwMiLn-CywQ155h6Vy6Mf9BD50J-_Gr_7iks6pXrzLEnNJpm_pkzMCUCAQEEIGaubOntgSILBe0tt5lWLxl3U653okZm7dkquW0gXusJ#Tanner_Nelson_Resume' ) ;
+
+?>
